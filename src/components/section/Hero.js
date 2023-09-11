da56196d187b7a0e9696509acc7bfcf3f7642312
@FerrerMarkAnthony <markferrer0910@gmail.com>
2023-09-11 09:12:17 +0800

    feat: adding button in 2xl screen

--- src/components/section/Hero.js
+++ src/components/section/Hero.js
@@ -23,18 +23,20 @@ const Hero = () => {
               />
             </span>
 
-            <div className="para text-center md:text-left font-creepster text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl">
+            <div className="para font-creepster text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl">
               <p>explore the world with your favorite characters.</p>
             </div>
 
             <div>
-              <p className="para2 text-center lg:text-left font-jakarta text-sm md:max-w-xs lg:max-w-xl 2xl:max-w-4xl md:text-base lg:text-lg 2xl:text-xl">
+              <p className="para2 lg:text-left font-jakarta text-sm md:max-w-xs lg:max-w-xl 2xl:max-w-4xl md:text-base lg:text-lg 2xl:text-xl">
                 In the MMORPG game Monyz, there are more than millions of
                 characters with different hero roles that you can use to explore
                 and get rare items by killing monsters and monster kings.
               </p>
             </div>
-            <div className="card hero-bg p-24 md:p-36 lg:p-42 xl:p-42 w-full lg:w-3/5 2xl:w-1/2 mt-10"></div>
+            <div className="relative card hero-bg p-24 md:p-36 lg:p-42 xl:p-44 w-full lg:w-3/5 2xl:max-w-2xl mt-10">
+              <img className="hero-button absolute" src="/button.png" alt="" />
+            </div>
           </div>
           <div className="absolute top-80 lg:top-48 md:top-28 xl:top-56 2xl:top-36">
             <Image className="image object-contain" src="/monsterbg.png" fill />
