64f84a2243bb0cfede8f2426176d12803ff45195
@FerrerMarkAnthony <markferrer0910@gmail.com>
2023-09-15 06:18:44 +0800

    feat: pre-footer cleaning code

--- src/components/PreFooter.js
+++ src/components/PreFooter.js
@@ -2,20 +2,21 @@ import React from "react";
 import Image from "next/image";
 
 const PreFooter = () => {
+  const [{ title: preFooterTitle, subtitle: preFooterSubtitle }] = [
+    {
+      title: "Embark on your adventure.",
+      subtitle:
+        " Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
+    },
+  ];
   return (
-    <section className="p-10">
-      <div className="pre-footer mx-auto w-10/12 md:max-w-xl lg:max-w-4xl xl:max-w-5xl 2xl:max-w-screen-2xl">
-        <div className="relative flex items-center justify-end p-4 md:p-6 lg:p-10 xl:p-14 2xl:p-28">
+    <section className="pre-footer__container">
+      <div className="pre-footer__container-content">
+        <div className="relative pre-footer__container-image">
           <div className="three-eye absolute md:w-4/12 bottom-0 md:left-3 lg:left-0 xl:left-20 h-full lg:w-5/12"></div>
-          <div className="md:max-w-xs lg:max-w-sm xl:max-w-md 2xl:max-w-2xl md:text-left text-center">
-            <h1 className="pre-footer-font font-creepster text-3xl md:text-3xl lg:text-5xl xl:text-7xl 2xl:text-8xl ">
-              Embark on your adventure.
-            </h1>
-            <p className="mt-5 md:mt-0 pre-footer-font font-jakarta text-sm md:text-sm lg:text-base max-w-xl">
-              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-              amet sint. Velit officia consequat duis enim velit mollit.
-              Exercitation veniam consequat sunt nostrud amet.
-            </p>
+          <div className="pre-footer__text-container">
+            <h1 className="pre-footer-title">{preFooterTitle}</h1>
+            <p className="pre-footer-subtitle">{preFooterSubtitle}</p>
           </div>
         </div>
       </div>
