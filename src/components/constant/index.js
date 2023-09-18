b51c47662b96e0044183fdf4a2445fe056689a2c
@FerrerMarkAnthony <markferrer0910@gmail.com>
2023-09-18 11:49:42 +0800

    feat: hero cons update

---
+++ src/components/constant/index.js
@@ -0,0 +1,13 @@
+export const heroDetails = [
+  {
+    title: "MMORP MOBILE",
+    variant: "font-jakarta primary-btn-sm text-sm lg:text-base py-1 px-2",
+  },
+  {
+    title: "explore the world with your favorite characters.",
+    subtitle:
+      "In the MMORPG game Monyz, there are more than millions of characters with different hero roles that you can use to explore and get rare items by killing monsters and monster kings.",
+    imageUrlButton: "/playbutton.png",
+    imageUrlMonster: "/monster.png",
+  },
+];
--- src/components/section/Hero.js
+++ src/components/section/Hero.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Image from "next/image";
+import NavbarButton from "../NavbarButton";
 import CustomButton from "../CustomButton";
 import CustomBar from "../CustomBar";
-import NavbarButton from "../NavbarButton";
+import { heroDetails } from "../constant";
 
 const Hero = () => {
   const [
@@ -13,19 +14,7 @@ const Hero = () => {
       imageUrlButton: playButtonImage,
       imageUrlMonster: monsterImage,
     },
-  ] = [
-    {
-      title: "MMORP MOBILE",
-      variant: "font-jakarta primary-btn-sm text-sm lg:text-base py-1 px-2",
-    },
-    {
-      title: "explore the world with your favorite characters.",
-      subtitle:
-        "In the MMORPG game Monyz, there are more than millions of characters with different hero roles that you can use to explore and get rare items by killing monsters and monster kings.",
-      imageUrlButton: "/playbutton.png",
-      imageUrlMonster: "/monster.png",
-    },
-  ];
+  ] = heroDetails;
 
   return (
     <section>
